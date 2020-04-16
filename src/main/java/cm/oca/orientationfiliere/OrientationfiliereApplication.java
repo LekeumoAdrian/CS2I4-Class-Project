05cad84b4a47f3aea2e6a6fc8d265201771ecadf
Adrian LEKEUMO <adrian.lekeumo@2tcorp.org>
2020-04-16 15:24:07 +0100

    premier commit initialisation du projet

---
+++ src/main/java/cm/oca/orientationfiliere/OrientationfiliereApplication.java
@@ -0,0 +1,13 @@
+package cm.oca.orientationfiliere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrientationfiliereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrientationfiliereApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cm/oca/orientationfiliere/OrientationfiliereApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.oca.orientationfiliere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrientationfiliereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
