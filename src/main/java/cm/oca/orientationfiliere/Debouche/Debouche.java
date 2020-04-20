f1b774c5950d205418e87bda1c2d3b5f3faae3b4
Adrian LEKEUMO <adrian.lekeumo@2tcorp.org>
2020-04-20 10:51:11 +0100

    FIX issue:Added relation into debouche and diplome

--- src/main/java/cm/oca/orientationfiliere/Debouche/Debouche.java
+++ src/main/java/cm/oca/orientationfiliere/Debouche/Debouche.java
@@ -1,16 +1,18 @@
 package cm.oca.orientationfiliere.Debouche;
 
+import cm.oca.orientationfiliere.Diplome.Diplome;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
-
+import java.util.List;
 
 
 @Data
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 @AllArgsConstructor
 @Entity
 
@@ -18,6 +20,16 @@ public class Debouche implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idDebouche;
+
+    @NotNull
     private String name;
+
+    @NotNull
     private String description;
+
+    @ManyToMany(mappedBy = "debouches")
+    private List<Diplome> diplomes;
+
+    public Debouche() {
+    }
 }
--- src/main/java/cm/oca/orientationfiliere/Diplome/Diplome.java
+++ src/main/java/cm/oca/orientationfiliere/Diplome/Diplome.java
@@ -1,12 +1,14 @@
 package cm.oca.orientationfiliere.Diplome;
 
+import cm.oca.orientationfiliere.Debouche.Debouche;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
-
+import java.util.List;
 
 
 @Data
@@ -17,7 +19,15 @@ import java.io.Serializable;
 public class Diplome implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+
     private Long idDiplome;
+
+    @NotNull
     private String intituleDiplome;
+    @NotNull
     private String specialite;
+
+    @ManyToMany
+    private List<Debouche> debouches;
 }
